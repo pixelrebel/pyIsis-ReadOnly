543fe144f3fa9ca16ddf4d85576fa41f049a4ac5
pixelrebel <github@pixelrebel.com>
2016-01-05 14:18:17 -0800

    Minor tweaks for pypi.

--- setup.py
+++ setup.py
@@ -7,29 +7,32 @@ def readme():
         return f.read()
 
 setup(name='pyIsis_ReadOnly',
-      version=__version__,
-      description='Python wrapper for Avid Isis Client Management Console',
-      long_description=readme(),
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: MacOS X',
-          'Intended Audience :: Information Technology',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: MIT License',
-          'Natural Language :: English',
-          'Programming Language :: Python',
-          'Topic :: System :: Systems Administration',
-          'Topic :: System :: Filesystems',
-          'Topic :: Utilities'
-      ],
-      keywords='avid isis client mount workspace',
-      url='http://github.com/pixelrebel/pyIsis_ReadOnly',
-      author='Sylvain Maziere',
-      author_email='sylvain@predat.fr',
-      license='MIT',
-      packages=['pyIsis_ReadOnly'],
-      include_package_data=True,
-      install_requires=['osa','xmltodict'],
-      zip_safe=False)
-
+    version=__version__,
+    description='Python wrapper for Avid Isis Client Management Console',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: MacOS X',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities'
+    ],
+    keywords='avid isis client mount workspace',
+    author='Sylvain Maziere',
+    author_email='sylvain@predat.fr',
+    maintainer='Jamie Evans',
+    maintainer_email='github@pixelrebel.com',
+    license='MIT',
+    packages=['pyIsis_ReadOnly'],
+    include_package_data=True,
+    install_requires=['osa','xmltodict'],
+    url='https://github.com/pixelrebel/pyIsis_ReadOnly',
+    download_url='https://github.com/pixelrebel/pyIsis_ReadOnly/tarball/v0.2.1',
+    zip_safe=False)
+    
 
